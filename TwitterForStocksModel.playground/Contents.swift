1ba08089daca5684f839c9a4ed8ca0df193141ad
AdityaAmbekar <02aditya96@gmail.com>
2020-09-20 19:35:08 +0530

    successfully trained and added the mlmodel

---
+++ TwitterForStocksModel.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+import Cocoa
+import CreateML
+
+let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/aditya/Desktop/Folder/IOS/TwitterForStocks/twitter-sanders-apple3.csv"))
+
+//Training on 70% data and randomizing with seed value 5 which yeilds max accuracy
+let(trainingData, testingData) = data.randomSplit(by: 0.7, seed: 5)
+
+//Using classifier on training data
+let sentimentClassifier = try MLTextClassifier(trainingData: trainingData, textColumn: "text", labelColumn: "class")
+
+//testing on Testing data
+let evaluationMetrics = sentimentClassifier.evaluation(on: testingData, textColumn: "text", labelColumn: "class")
+
+//Accuracy check
+let evalAccuracy = (1 - evaluationMetrics.classificationError) * 100
+
+//Metadata
+let metadata = MLModelMetadata(author: "Aditya Ambekar",
+                               shortDescription: "A model to classify whether the tweet is negative or positive",
+                               license: nil,
+                               version: "1.0.0")
+
+//Creating the model
+try sentimentClassifier.write(to: URL(fileURLWithPath: "/Users/aditya/Desktop/Folder/IOS/TwitterForStocks/TweetSentimentClassifier.mlmodel"))
+
+
+
+//checking if it is predicting right or not
+//try sentimentClassifier.prediction(from: "@apple is good though")
